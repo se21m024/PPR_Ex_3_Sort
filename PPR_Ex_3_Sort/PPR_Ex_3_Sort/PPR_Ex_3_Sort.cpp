6c858d578ae2d05ef121c2b39c9a2bb9a5f7f14e
Thomas Stummer <thomas.stummer@grassfish.com>
2022-03-13 18:45:05 +0100

    add project

---
+++ PPR_Ex_3_Sort/PPR_Ex_3_Sort/PPR_Ex_3_Sort.cpp
@@ -0,0 +1,16 @@
+/*
+	Parallel Programming
+	Exercise 3
+	Sorting Algorithms
+
+	Student 1: Thomas Bründl
+	Student 2: Thomas Stummer
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
